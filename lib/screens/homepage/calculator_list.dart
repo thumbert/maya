d39e5407df53a68020f43b6bd2ed0dde735fbe04
thumbert <tony.humberts@gmail.com>
2021-02-07 20:01:16 -0500

    add arrow to calculator list

--- lib/screens/homepage/calculator_list.dart
+++ lib/screens/homepage/calculator_list.dart
@@ -11,6 +11,9 @@ class CalculatorList extends StatefulWidget {
 
 class _CalculatorListState extends State<CalculatorList> {
   _CalculatorListState();
+
+  int _hoveringIndex;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,24 +22,42 @@ class _CalculatorListState extends State<CalculatorList> {
         child: ListView(
           padding: EdgeInsets.all(20.0),
           children: List.generate(calculators.length, (index) {
-            return ListTile(
-              onTap: () => _onReportTap(context, index),
-              selected: calculators[index].selected,
-              leading: GestureDetector(
-                behavior: HitTestBehavior.opaque,
-                onTap: () => _onReportTap(context, index),
-                child: Container(
-                  width: 48,
-                  height: 48,
-                  alignment: Alignment.center,
-                  child: CircleAvatar(
-                    backgroundColor: calculators[index].color,
+            return MouseRegion(
+                onEnter: (_) {
+                  setState(() {
+                    _hoveringIndex = index;
+                  });
+                },
+                onExit: (_) {
+                  setState(() {
+                    _hoveringIndex = null;
+                  });
+                },
+                child: ListTile(
+                  onTap: () => _onReportTap(context, index),
+                  onLongPress: () => _onReportTap(context, index),
+                  selected: calculators[index].selected,
+                  leading: GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onTap: () => _onReportTap(context, index),
+                    child: Container(
+                      width: 48,
+                      height: 48,
+                      alignment: Alignment.center,
+                      child: CircleAvatar(
+                        backgroundColor: calculators[index].color,
+                        child: _hoveringIndex == index
+                            ? Icon(
+                                Icons.arrow_forward,
+                                color: Colors.black26,
+                              )
+                            : null,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              title: Text(calculators[index].title),
-              // subtitle: Text(paints[index].title),
-            );
+                  title: Text(calculators[index].title),
+                  // subtitle: Text(paints[index].title),
+                ));
           }),
         ));
   }
